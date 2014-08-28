603fcfd6e09675da2d694bf425dbd1b4cebea2ce
mrkiffie <mrkiffie@gmail.com>
2014-08-29 00:37:26 +0200

    Extending functionality
    - added deffered ajax calling via a cors enabled proxy
    - added form for advanced filtering

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,57 @@
 /*jslint indent: 2 */
 /*global window: true, document: true, jQuery: true, Modernizr: true, localStorage: true*/
 'use strict';
+
+/**
+ * Adds a filter to all AJAX requests, sending them through the CORS proxy
+ */
+jQuery.ajaxPrefilter(function(options) {
+
+  if (options.crossDomain && jQuery.support.cors) {
+    options.url = (window.location.protocol === 'http:' ? 'http:' : 'https:') +
+      '//cors-anywhere.herokuapp.com/' + options.url;
+    }
+
+});
+
 jQuery(document).ready(function ($) {
+
+  /**
+   * A simple localStorage abstraction
+   */
+  var cache = {
+
+    get : function (key) {
+      return JSON.parse(localStorage.getItem(key));
+    },
+
+    set : function (key, data) {
+      return localStorage.setItem(key, JSON.stringify(data));
+    }
+
+  };
+
+  /**
+   * Formats the date d mmmm yyyy @ hh:mm
+   */
+  var formatDate = function (date){
+
+    var regex = /(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/,
+      dateArray = regex.exec(date),
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+      // d M Y @ H:i
+      return parseInt(dateArray[3],10) + ' ' + months[dateArray[2]-1] + ' ' + dateArray[1] + ' @ ' + dateArray[4] + ':' + dateArray[5];
+
+  }
+
   var earthquakeStat = {
 
     config: {
 
       localStorage: 'earthquakeStatData',
 
-      endPoint: '/api/service.json'
+      endPoint: 'http://www.seismi.org/api/eqs/'
 
     },
 
@@ -17,49 +60,101 @@ jQuery(document).ready(function ($) {
     data: {},
 
 
-    loadData: function () {
+    loadData: function (endPoint) {
+
+      var dfd = $.Deferred();
 
       $.ajax({
-        url: earthquakeStat.config.endPoint,
+        url: endPoint,
         success: function (response) {
 
           earthquakeStat.data = response;
-
-          localStorage.setItem(earthquakeStat.config.localStorage, JSON.stringify(earthquakeStat.data));
-
+          dfd.resolve(response);
+          cache.set(endPoint, response);
         },
         dataType: 'json'
       });
-
+      return dfd.promise();
     },
 
-    loadMap : function (event) {
-      var img = $(this).siblings('img');
-
+    doNothing : function (event){
       event.preventDefault();
+    },
+    buildQuery : function (){
+
+      var year, month, limit, min_magnitude, query, path, querystring;
+
+      year = $('#year').val();
+      month = $('#month').val();
+      limit = $('#limit').val();
+      min_magnitude = $('#min_magnitude').val();
+
+      if ( year !== ''){
+        path = year;
+        if (month >= 1 && month <= 12){
+          path = path + '/' + month;
+        }
+      }
+
+      querystring = {};
+      // the API's limit is 900
+      if (limit > 1 && limit < 900 ) {
+
+        querystring.limit = limit;
+      }
+      if (min_magnitude > 1 && min_magnitude < 10 ) {
+        querystring.min_magnitude = min_magnitude;
+      }
+      querystring = $.param(querystring);
+
+      query = path + '?' + querystring;
+
+      return query;
+    },
+
+    loadMap : function () {
+      var img = $('<img/>'),
+        parent = $(this).parent(),
+        src = parent.attr('data-map-src');
 
       $(this).attr('disabled', 'disabled');
 
-      img.attr('width', 800)
+      img.attr('src', src)
+        .attr('width', 800)
         .attr('height', 800);
-      img.attr('src', img.attr('data-src'));
+
+      img.insertAfter(parent.find('h2'));
+    },
+
+    getData: function () {
+      var dfd = $.Deferred(),
+        endPoint = earthquakeStat.config.endPoint + earthquakeStat.buildQuery();
+
+      if (earthquakeStat.localStorage) {
+        earthquakeStat.data = cache.get(endPoint);
+        if (earthquakeStat.data === null) {
+          $.when(earthquakeStat.loadData(endPoint)).done(dfd.resolve);
+        }
+      } else {
+        $.when(earthquakeStat.loadData(endPoint)).done(dfd.resolve);
+      }
+      return dfd.promise();
 
     },
 
     init: function () {
 
-      if (earthquakeStat.localStorage) {
-        earthquakeStat.data = JSON.parse(localStorage.getItem(earthquakeStat.config.localStorage));
-        if (earthquakeStat.data === null) {
-          earthquakeStat.loadData();
-        }
-      } else {
-        earthquakeStat.loadData();
-      }
-
-      earthquakeStat.renderFeed();
+      earthquakeStat.loadDataSet();
 
       $('#feed').on('click', 'button', earthquakeStat.loadMap);
+      $('#controls').on('click', '#submit', earthquakeStat.doNothing);
+      $('#controls').on('click', '#submit', earthquakeStat.loadDataSet);
+    },
+
+    loadDataSet : function () {
+
+      $.when(earthquakeStat.getData()).done(earthquakeStat.renderFeed);
+
     },
 
 
@@ -67,34 +162,28 @@ jQuery(document).ready(function ($) {
       var fragment = document.createDocumentFragment(),
         li,
         region,
-        img,
         magnitude,
         time,
         depth,
-        coods,
+        coords,
         button;
 
       $.each(earthquakeStat.data.earthquakes, function (i) {
 
+        coords = earthquakeStat.data.earthquakes[i].lat + ',' + earthquakeStat.data.earthquakes[i].lon;
+
         li = $('<li/>')
           .attr('data-lat', earthquakeStat.data.earthquakes[i].lat)
           .attr('data-lon', earthquakeStat.data.earthquakes[i].lon)
+          .attr('data-map-src', 'http://maps.googleapis.com/maps/api/staticmap?center=' + coords + '&markers=size:mid%7Ccolor:red%7C' + coords + '&zoom=3&scale=2&size=400x400&maptype=hybrid&format=png')
           .addClass('cf');
 
         region = $('<h2/>')
           .text(earthquakeStat.data.earthquakes[i].region === '.' ? '(location not provided)' : earthquakeStat.data.earthquakes[i].region);
 
-        coods = earthquakeStat.data.earthquakes[i].lat + ',' + earthquakeStat.data.earthquakes[i].lon;
-
-        img = $('<img/>')
-          .attr('data-src', 'http://maps.googleapis.com/maps/api/staticmap?center=' + coods + '&markers=size:mid%7Ccolor:red%7C' + coods + '&zoom=3&scale=2&size=400x400&maptype=hybrid&format=png')
-          .attr('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7')
-          .attr('width', 800)
-          .attr('height', 800);
-
         time = $('<time/>')
           .attr('datetime', earthquakeStat.data.earthquakes[i].timedate)
-          .text(earthquakeStat.data.earthquakes[i].timedate);
+          .text(formatDate(earthquakeStat.data.earthquakes[i].timedate));
 
         magnitude = $('<span/>')
           .addClass('magnitude')
@@ -108,7 +197,6 @@ jQuery(document).ready(function ($) {
           .text('Load Map');
 
         li.append(region)
-          .append(img)
           .append(time)
           .append(magnitude)
           .append(depth)
@@ -117,7 +205,7 @@ jQuery(document).ready(function ($) {
         li.appendTo(fragment);
       });
 
-      $('#feed').append(fragment);
+      $('#feed').empty().append(fragment);
     }
   };
 
